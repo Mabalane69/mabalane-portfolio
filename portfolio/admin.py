3e07bf22ef7728fe12f2e02cb60aedc0daac6ab5
Mabalane Aphane <MAPHANE@ackermans.co.za>
2026-09-23 15:44:54 +0200

    admin is ready. API portfolio or projects is not pulling

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'technologies',
+        'featured',
+        'created_at',
+    )
+
+    list_filter = (
+        'featured',
+        'created_at',
+    )
+
+    search_fields = (
+        'title',
+        'description',
+        'technologies',
+    )
---
+++ portfolio/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ProjectListAPIView
+
+
+urlpatterns = [
+    path('projects/', ProjectListAPIView.as_view(), name='project-list'),
+]
