b223d0fe85d169111cffe260732b8575ab08790b
Mabalane Aphane <MAPHANE@ackermans.co.za>
2026-09-23 11:55:14 +0200

    project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technologies = models.CharField(max_length=500)
+    github_url = models.URLField(blank=True)
+    image_url = models.URLField(blank=True)
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
