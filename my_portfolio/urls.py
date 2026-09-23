5fdbcac05e06e1868fde7e1d3e64043c645fc95f
Mabalane Aphane <MAPHANE@ackermans.co.za>
2026-09-23 15:57:17 +0200

    API resolved getting projects now

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 from django.urls import include, path
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/projects/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+
+class ProjectListAPIView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
